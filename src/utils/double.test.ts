424bc8c02db21409c3672e5847e530c1eb7aa383
Roiner Camacho <rocaes90@gmail.com>
2023-04-17 16:14:22 -0600

    test is going to fail

--- src/utils/double.test.ts
+++ src/utils/double.test.ts
@@ -3,6 +3,6 @@ import { double } from './double'
 describe('testing double function', () => {
   test('should multiply by two', () => {
     const result = double({ value: 5 })
-    expect(result).toBe(10)
+    expect(result).toBe(5)
   })
 })
